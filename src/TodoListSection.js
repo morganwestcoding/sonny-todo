362c461468bbd5b005e49288e5b94d1c2f9586a2
Morgan West <mewwes@gmail.com>
2023-06-04 04:18:28 -0700

    Button check + trash add

--- src/TodoListSection.js
+++ src/TodoListSection.js
@@ -1,33 +1,66 @@
-import React, {useState} from 'react'
-import "./App.css";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faCheck } from '@fortawesome/free-solid-svg-icons';
+import './App.css';
 
 function TodoListSection() {
-  const [todos,setTodos] = useState([]);
-  const [input,setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  const [completed, setCompleted] = useState([]);
 
-  const addTodo =(e) => {
-    e.preventDefault();
+  const addTodo = () => {
     console.log(`this is the input ${input}`);
     setTodos([...todos, input]);
-    setInput("");
-  }
+    setInput('');
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  const toggleCompleted = (index) => {
+    const updatedCompleted = [...completed];
+    if (updatedCompleted.includes(index)) {
+      updatedCompleted.splice(updatedCompleted.indexOf(index), 1);
+    } else {
+      updatedCompleted.push(index);
+    }
+    setCompleted(updatedCompleted);
+  };
+
   return (
     <div>
       <h1>Today's TODOs</h1>
-      <form>
-         <input value={input}
-      onChange={(e) => setInput(e.target.value)} 
-      type="text"></input>
-
-      <button type="submit" onClick={addTodo}>Add to List</button>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        />
+        <button type="submit" onClick={addTodo}>
+          Add to List
+        </button>
       </form>
 
       <h2>List of Todos</h2>
-      {todos.map((todo) => (
-        <p>{todo}</p>
+      {todos.map((todo, index) => (
+        <div key={index} className={`todo ${completed.includes(index) ? 'completed' : ''}`}>
+          <p>
+            <span className="task" onClick={() => toggleCompleted(index)}>
+              {todo}
+            </span>
+            <span className="icons">
+              <FontAwesomeIcon icon={faCheck} onClick={() => toggleCompleted(index)} />
+              <span className="icon-spacing" />
+              <FontAwesomeIcon icon={faTrashAlt} onClick={() => removeTodo(index)} />
+            </span>
+          </p>
+        </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default TodoListSection;
